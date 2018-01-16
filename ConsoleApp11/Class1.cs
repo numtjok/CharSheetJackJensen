334926dfa44f6733b49d2b3886c44142714432fa
DESKTOP-OMKBHQ9\Jack <numtjok1234@gmail.com>
2018-01-16 14:03:38 +0100

    no message

---
+++ ConsoleApp11/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    class Class1
+    {
+        private string firstname;
+        private string lastname;
+        private int age;
+        private string alignment;
+        private string race;
+        private string backstory;
+
+        public string FirstName {
+            get { return firstname; }
+            set { firstname = value; }
+
+        }
+
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+
+        }
+
+        public string Alignment
+        {
+            get { return alignment; }
+            set { alignment = value; }
+
+        }
+
+        public string Race
+        {
+            get { return race; }
+            set { race = value; }
+
+        }
+
+        public string Backstory
+        {
+            get { return backstory; }
+            set { backstory = value; }
+
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            Console.WriteLine("Please enter the first name of your Character:");
+            class1.FirstName = Console.ReadLine();
+            Console.WriteLine("Please enter the last name of your Character");
+            class1.LastName = Console.ReadLine();
+            Console.WriteLine("Please enther the age of you Character");
+            class1.Age = Int32.Parse(Console.ReadLine());
+
+        }
+    }
+}
